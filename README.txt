6ee0ea9a6959e0bfa6aa72d4a16486a88c89966d
mako <uwu@sunflowering.pw>
2023-11-08 17:24:32 +0000

    ditch switch

--- README.txt
+++ README.txt
@@ -6,18 +6,11 @@ or activate them when they don't spin after boot sequence, it currently
 does not support dual or multiple slots and only a single card can be
 instructed to do its task.
 
-usage:
-sudo hwhl
+In session:
++ Temp Monitor
++ 85°C Overheat Protection
+
+Usage:
+hwhl, hwhl -c <0-255>
 
 duhhh.
-
-Additions:
-+ 2023-11-06: [?]
-
-automatically calls poweroff
-when junction rises to 85°C
-
-this safety feature is only
-active if the user picks the
-second option in the starting
-screen and is kept open.
--- hwhl.c
+++ hwhl.c
@@ -21,17 +21,15 @@ void tempGPU() {
         int val[3];
 
         for (int i = 0; i < 3; i++) {
-            FILE *file = fopen(path[i], "r");
+            FILE *fh = fopen(path[i], "r");
 
-            if (file) {
-                if (fscanf(file, "%d", &val[i]) != 1) {
-                    perror("[xx]");
-                    exit(1);
-                }
-                fclose(file);
-
+            if (fscanf(fh, "%d", &val[i]) == 1) {
                 val[i] /= 1000;
+            } else {
+                fprintf(stderr, "%s\n", "[x]");
+                exit(1);
             }
+            fclose(fh);
         }
 
 	printf("\033[2J\033[1r");
@@ -40,79 +38,62 @@ void tempGPU() {
             printf("%-12s %d°C\n", label[i], val[i]);
         }
 
-        fflush(stdout);
-
-	/* check for junction temp */
         if (val[1] >= 85) {
-            system("poweroff");
-	    break;
+            if (system("poweroff") == -1) {
+                exit(1);
+            }
+            break;
         }
 
         sleep(4);
     }
 }
 
-void fancGPU() {
+void fancGPU(const char *str) {
     const char *path[] = {
         "/sys/class/drm/card0/device/hwmon/hwmon0/pwm1_enable",
         "/sys/class/drm/card0/device/hwmon/hwmon0/pwm1",
     };
 
-    printf("[0-255]: ");
+    char *end;
+    short val = strtol(str, &end, 10);
 
-    int val;
-
-    if (scanf("%d", &val) != 1 || val < 0 || val > 255) {
-        printf("[x]\n");
+    /* eliminate char string input,
+       set out-of-range integer values */
+    if (*end != '\0' || val < 0 || val > 255) {
+        fprintf(stderr, "%s\n", "[x]");
         exit(1);
     }
 
     for (int i = 0; i < 2; i++) {
         if (chmod(path[i], 0644) < 0) {
-            perror("[xx]");
+            fprintf(stderr, "%s\n", "[x]");
             exit(1);
         }
+        FILE *fh = fopen(path[i], "w");
+        fprintf(fh, i == 0 ? "1" : "%d", val);
 
-        /* submit pwm values directly to function
-           without any proper error handling */
-        FILE *file = fopen(path[i], "w");
-
-        if (i == 0) {
-            fprintf(file, "1");
-        } else {
-            fprintf(file, "%d", val);
-        }
-
-        fclose(file);
-
+        fclose(fh);
         chmod(path[i], 0444);
     }
 }
 
-int main() {
-    printf("\033[2J\033[1r");
-    printf("%s\n", "[1] GPU: Fan Control");
-    printf("%s\n", "[2] GPU: Fan Monitor");
-    printf("\n%s", "[?]: ");
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        tempGPU();
 
-    char options;
-    do {
-	scanf(" %c", &options);
-	printf("\n");
-
-        switch (options) {
-            case '1':
-                fancGPU();
-                break;
-            case '2':
-                tempGPU();
-                break;
-
-            default:
-                printf("\033[2J\033[1r");
-                break;
-        }
-    } while (options < '1' || options > '2');
+    /* lock to appropriate use
+       of arguments with hwhl -c */
+    } else if (argc == 3
+	    && argv[1][0] == '-'
+	    && argv[1][1] == 'c'
+            && (argv[1][2] == ' '
+	    || argv[1][2] == '\0')) {
+        fancGPU(argv[2]);
+    } else {
+        fprintf(stderr, "%s\n", "[-c 0-255]");
+        return 1;
+    }
 
     return 0;
 }
